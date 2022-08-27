7309573e6a45e9cef2a4f78581d2927cc88ccfa4
ritsuke <ritsuke@gmail.com>
2022-08-26 20:23:34 -0500

    New channels for presence and frontpage - Separate pub and sub clients - Update package name

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import fastifySocketIOPlugin from 'fastify-socket.io'
 import redis from 'redis'
+import type { Socket } from 'socket.io'
 
 if (!process.env.PORT) throw 'missing port; check env'
 if (!process.env.REDIS_DB_URL) throw 'missing redis db url; check env'
 
 const HOST = '0.0.0.0',
   PORT = parseInt(process.env.PORT),
-  COMMENT_STREAM_CHANNEL_NAME = 'MEATBALLS:COMMENT_STREAM'
+  PRESENCE_STREAM_CHANNEL_KEY = 'MEATBALLS:PRESENCE_STREAM',
+  COMMENT_STREAM_CHANNEL_KEY = 'MEATBALLS:COMMENT_STREAM',
+  FRONTPAGE_STREAM_CHANNEL_KEY = 'MEATBALLS:FRONTPAGE_STREAM'
 
-const redisClient = redis.createClient({ url: process.env.REDIS_DB_URL })
+const redisPubClient = redis.createClient({ url: process.env.REDIS_DB_URL }),
+  redisSubClient = redis.createClient({ url: process.env.REDIS_DB_URL })
 
 const app = fastify()
 
@@ -30,19 +34,43 @@ app.get('/', (_, res) => {
 app.ready(async (error) => {
   if (error) throw error
 
-  await redisClient.connect()
+  await Promise.all([redisPubClient.connect(), redisSubClient.connect()])
 
   console.info(`[KODAMA] ready; listening on port ${PORT}`)
 
-  redisClient.subscribe(COMMENT_STREAM_CHANNEL_NAME, (message: string) => {
-    app.io.emit(COMMENT_STREAM_CHANNEL_NAME, message)
+  redisSubClient.subscribe(PRESENCE_STREAM_CHANNEL_KEY, (message: string) => {
+    console.info(`[KODAMA:${PRESENCE_STREAM_CHANNEL_KEY}]: ${message}`)
+
+    app.io.emit(PRESENCE_STREAM_CHANNEL_KEY, message)
   })
 
-  app.io.on('connection', (socket: any) => {
-    console.info(`${socket.id} connected...`)
+  redisSubClient.subscribe(COMMENT_STREAM_CHANNEL_KEY, (message: string) => {
+    console.info(`[KODAMA:${COMMENT_STREAM_CHANNEL_KEY}]: ${message}`)
+
+    app.io.emit(COMMENT_STREAM_CHANNEL_KEY, message)
+  })
+
+  redisSubClient.subscribe(FRONTPAGE_STREAM_CHANNEL_KEY, (message: string) => {
+    console.info(`[KODAMA:${FRONTPAGE_STREAM_CHANNEL_KEY}]: ${message}`)
+
+    app.io.emit(FRONTPAGE_STREAM_CHANNEL_KEY, message)
+  })
+
+  app.io.on('connection', (socket: Socket) => {
+    console.info(`[KODAMA:connect]: ${socket.id}`)
+
+    redisPubClient.publish(
+      PRESENCE_STREAM_CHANNEL_KEY,
+      `{ "users": ${app.io.engine.clientsCount as number} }`
+    )
 
     socket.on('disconnect', () => {
-      console.info(`${socket.id} disconnected...`)
+      redisPubClient.publish(
+        PRESENCE_STREAM_CHANNEL_KEY,
+        `{ "users": ${app.io.engine.clientsCount as number} }`
+      )
+
+      console.info(`[KODAMA:disconnect]: ${socket.id}`)
     })
   })
 })
